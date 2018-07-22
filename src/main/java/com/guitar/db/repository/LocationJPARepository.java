a5b6d6d856ef7aafbbb73f9f07f98149db07f638
Tmsnoo <tomek.laskawiec@gmail.com>
2018-07-22 07:13:32 +0200

    Add DSL specific query and tested them

--- src/main/java/com/guitar/db/repository/LocationJPARepository.java
+++ src/main/java/com/guitar/db/repository/LocationJPARepository.java
@@ -1,10 +1,17 @@
 package com.guitar.db.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.guitar.db.model.Location;
+
 @Repository
 public interface LocationJPARepository extends JpaRepository<Location, Long> {
-
+	List<Location> findByStateLike(String stateName);
+	
+	List<Location> findByStateOrCountry(String state, String Country);
+	
+	List<Location> findByStateAndCountry(String state , String country);
 }
--- src/test/java/com/guitar/db/LocationPersistenceTests.java
+++ src/test/java/com/guitar/db/LocationPersistenceTests.java
@@ -1,10 +1,15 @@
 package com.guitar.db;
 
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -23,8 +28,6 @@ import com.guitar.db.repository.LocationRepository;
 @ContextConfiguration(locations={"classpath:com/guitar/db/applicationTests-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class LocationPersistenceTests {
-	@Autowired
-	private LocationRepository locationRepository;
 	
 	@Autowired
 	LocationJPARepository locationJPARepository;
@@ -38,36 +41,53 @@ public class LocationPersistenceTests {
 		List<Location> location = locationJPARepository.findAll();
 		assertThat(location, notNullValue()); 
 	}
+	
+	@Test
+	public void testJpaRepositoryFindByStateOrCountry() {
+		//given
+		List<Location> locations = locationJPARepository.findByStateOrCountry("Utah", "Utah");
+		//when
+		List <String>location = locations.stream().map(l -> l.getState()).collect(Collectors.toList()); 
+		//then
+		assertThat(location.get(0), is("Utah"));
+	}
+	@Test
+	public void shouldFindByStateAndCountry() {
+		//given
+		List<Location> locations = locationJPARepository.findByStateAndCountry("Utah", "United States");
+		//expected
+		assertThat(locations.get(0).getCountry(), is("United States"));
+	}
 	@Test
 	@Transactional
 	public void testSaveAndGetAndDelete() throws Exception {
 		Location location = new Location();
 		location.setCountry("Canada");
 		location.setState("British Columbia");
-		location = locationRepository.create(location);
+		location = locationJPARepository.saveAndFlush(location);
 		
 		// clear the persistence context so we don't return the previously cached location object
 		// this is a test only thing and normally doesn't need to be done in prod code
 		entityManager.clear();
 
-		Location otherLocation = locationRepository.find(location.getId());
-		assertEquals("Canada", otherLocation.getCountry());
+		Location otherLocation = locationJPARepository.findOne(location.getId());
+		assertThat(otherLocation.getCountry(),is("Canada"));
 		assertEquals("British Columbia", otherLocation.getState());
 		
 		//delete BC location now
-		locationRepository.delete(otherLocation);
+		locationJPARepository.delete(otherLocation);
 	}
 
 	@Test
 	public void testFindWithLike() throws Exception {
-		List<Location> locs = locationRepository.getLocationByStateName("New");
-		assertEquals(4, locs.size());
+		List<Location> locs = locationJPARepository.findByStateLike("New%");
+		assertThat(locs, hasSize(4));
 	}
 
 	@Test
 	@Transactional  //note this is needed because we will get a lazy load exception unless we are in a tx
 	public void testFindWithChildren() throws Exception {
-		Location arizona = locationRepository.find(3L);
+		Location arizona = locationJPARepository.findOne(3L);
 		assertEquals("United States", arizona.getCountry());
 		assertEquals("Arizona", arizona.getState());
 		
